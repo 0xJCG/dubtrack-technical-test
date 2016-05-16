71c6aafedeb0f396756a982bfac24f2b437af3de
0xJCG <jonathancastrogonzalez@gmail.com>
2016-05-16 16:32:04 +0200

    Controllers and routing almost finished.

--- app.py
+++ app.py
@@ -1,18 +1,47 @@
 from flask import Flask
-from flask import request
 from flask import render_template
+from flask import request
 
-app = Flask(__name__)
+import requests
+
+app = Flask('technical-test')
+
+
+def get_channels():
+    url = 'https://api.dubtrack.fm/room'
+    response = requests.get(url)
+    if response.status_code == requests.codes.ok:
+        return True
+    return False
+
+
+def get_channel_info(channel):
+    url = 'https://api.dubtrack.fm/room' + channel
+    response = requests.get(url)
+    if response.status_code == requests.codes.ok:
+        return True
+    return False
+
 
 @app.route('/', method='POST')
 def index():
     if request.method == 'POST':
-        return render_template('search.html')
-    return render_template('index.html')
+        channel = get_channel_info(request.form['channel'])
+        if channel:
+            return render_template('search.html', channel=channel)
+        else:
+            return render_template('error.html')
+    channels = get_channels()
+    return render_template('index.html', channels=channels)
 
-@app.route('/channel/<int:ch_id>')
-def index():
-    return render_template('channel.html')
+
+@app.route('/<int:channel_id>')
+def show_channel_info(channel_id):
+    ch = get_channel_info(channel_id)
+    if ch:
+        return render_template('channel.html')
+    return render_template('error.html')
+
 
 if __name__ == '__main__':
     app.run()
